0c6af03563a1a2b5a9bb1782c3f3920f34fadf73
Fahim <fahim.cse.pust@outlook.com>
2023-07-24 13:10:51 +0600

    app.js editted

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("<h1>InShaAllah I Will Shine,<br>Game is not over until I Win</h1>");
+});
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+})
