474b1cd579235d4e7e25c60bd31462598fdfddb4
Fahim <fahim.cse.pust@outlook.com>
2023-07-24 16:44:29 +0600

    Again ejs related updatefor day name

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use("view-engine",ejs);
+app.set('view engine','ejs');
 
 app.get("/",function(req,res){
+    var today = new Date();
+    var currentDate = parseInt(today.getDate());
+    var day = "";
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+    const d = new Date();
+    day = weekday[d.getDay()];
+
+
+    res.render("list",{kindOfDay: day})
     res.send("<h1>InShaAllah I Will Shine,<br>Game is not over until I Win</h1>");
 });
 
