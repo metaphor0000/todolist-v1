afdb18310026218a75ae3a39b0a6e2e73f09901c
Fahim <fahim.cse.pust@outlook.com>
2023-07-27 18:02:55 +0600

    Intial Final

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var items = [];
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     var today = new Date();
